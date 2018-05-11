e90ab6364bf1a56a67038d03635a1ccc73e74c31
Radwa Ahmed Mohsen <radwa@gfxway.com>
2018-05-11 11:55:38 +0200

    matching cards

--- js/app.js
+++ js/app.js
@@ -26,12 +26,14 @@ console.log(ContentOfCards);
  let cards = document.querySelectorAll('.card');
 
  cards = toArray(cards);
-//display all cards
+//restart icon configuration
 const restartIcon = document.querySelector('.restart')
 restartIcon.addEventListener('click',function(){
 	for(let i=0 ; i<cards.length ; i++){
 	cards[i].classList.remove('open','show','match');
 }
+openCards = [];
+cardIndex = [];
 });
 
 //shuffle the cards
@@ -55,37 +57,93 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-let openCards = [] ;
-for(let i = 0 ;i< cards.length ;i++){
-cards[i].addEventListener('click',function(){
+
+ // * set up the event listener for a card. If a card is clicked:
+ // *  - display the card's symbol (put this functionality in another function that you call from this one)
+ // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ // *  - if the list already has another card, check to see if the two cards match
+ // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ // *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ 
+// let openCards = [] ;
+// for(let i = 0 ;i< cards.length ;i++){
+// cards[i].addEventListener('click',function(){
 	
-	if(openCards.length!==0){
-		if(openCards.length === 2){
-			openCards = [];
-         }
+// 	if(openCards.length!==0){
+// 		if(openCards.length === 2){
+			
+// 			openCards[0].classList.remove('open','show');
+// 			openCards[1].classList.remove('open','show');
+// 			openCards = [];
+//          }
          
-     	else if(cards[i].children[0] === openCards[0]){
-			cards[i].classList.add('match');
-			openCards[0].classList.remove('open','show');
-			openCards[0].classList.add('match');
-		}
-		else if (openCards.length<=1){
-			cards[i].classList.add('open','show');
-	openCards.push(cards[i].children[0]);
-		}
-	}
+//      	else if(cards[i].children[0] === openCards[0]){
+// 			cards[i].classList.add('match');
+// 			openCards[0].classList.remove('open','show');
+// 			openCards[0].classList.add('match');
+// 		}
+// 		else if (openCards.length<=1){
+// 			cards[i].classList.add('open','show');
+// 	openCards.push(cards[i].children[0]);
+// 		}
+// 	}
+// 	else{
+// 		cards[i].classList.add('open','show');
+// 	openCards.push(cards[i].children[0]);
+// 	}
     
 
+// })
+// }
+
+let openCards = [] ;
+let cardIndex = [] ;
+let match = false;
+for(let i = 0 ;i< cards.length ;i++){
+    cards[i].addEventListener('click',function(){
+	if(openCards.length!==0){
+		if(openCards.length === 2){
+			if(match === true){
+				openCards = [];
+				cardIndex = [];
+               cards[i].classList.add('open','show');
+	           openCards.push(cards[i].children[0].classList.item(1));
+	           cardIndex.push(i);
+	           match = false;
+	         }
+	        else{
+
+	        	cards[cardIndex[0]].classList.remove('open','show');
+			    cards[cardIndex[1]].classList.remove('open','show');
+
+			    openCards = [];
+			    cardIndex = [] ;
+               cards[i].classList.add('open','show');
+	           openCards.push(cards[i].children[0].classList.item(1));
+	           cardIndex.push(i);
+	        }
+		}
+		else if (openCards.length === 1){
+			cards[i].classList.add('open','show');
+			openCards.push(cards[i].children[0].classList.item(1));
+			cardIndex.push(i);
+			console.log(openCards[0]);
+			console.log(openCards[1]);
+            if(openCards[0] === openCards[1]){
+			cards[i].classList.add('match');
+			cards[cardIndex[0]].classList.remove('open','show');
+			cards[cardIndex[0]].classList.add('match');
+             match = true;
+		   }
+
+	    }
+	 }
+	else{
+		cards[i].classList.add('open','show');
+		openCards.push(cards[i].children[0].classList.item(1));
+		cardIndex.push(i);
+	}
 })
 }
-
