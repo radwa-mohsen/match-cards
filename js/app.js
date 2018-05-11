b74a3d53d2f09e242018adba416f78d342631088
Radwa Ahmed Mohsen <radwa@gfxway.com>
2018-05-11 16:27:49 +0200

    moves increment

--- js/app.js
+++ js/app.js
@@ -58,104 +58,22 @@ function shuffle(array) {
 
 
 
- // * set up the event listener for a card. If a card is clicked:
- // *  - display the card's symbol (put this functionality in another function that you call from this one)
- // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- // *  - if the list already has another card, check to see if the two cards match
- // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- // *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ // * set up the event listener for a card. If a card is clicked: done
+ // *  - display the card's symbol (put this functionality in another function that you call from this one) done
+ // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) done
+ // *  - if the list already has another card, check to see if the two cards match done
+ // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) done
+ // *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) done
  // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  
-// let openCards = [] ;
-// for(let i = 0 ;i< cards.length ;i++){
-// cards[i].addEventListener('click',function(){
-	
-// 	if(openCards.length!==0){
-// 		if(openCards.length === 2){
-			
-// 			openCards[0].classList.remove('open','show');
-// 			openCards[1].classList.remove('open','show');
-// 			openCards = [];
-//          }
-         
-//      	else if(cards[i].children[0] === openCards[0]){
-// 			cards[i].classList.add('match');
-// 			openCards[0].classList.remove('open','show');
-// 			openCards[0].classList.add('match');
-// 		}
-// 		else if (openCards.length<=1){
-// 			cards[i].classList.add('open','show');
-// 	openCards.push(cards[i].children[0]);
-// 		}
-// 	}
-// 	else{
-// 		cards[i].classList.add('open','show');
-// 	openCards.push(cards[i].children[0]);
-// 	}
-    
 
-// })
-// }
-
-// let openCards = [] ;
-// let cardIndex = [] ;
-// let match = false;
-// function holdCards(arrOfCards , index){
-// 	openCards.push(cards[i].children[0].classList.item(1));
-// 	cardIndex.push(i);
-// }
-// for(let i = 0 ;i< cards.length ;i++){
-//     cards[i].addEventListener('click',function(){
-// 	if(openCards.length!==0){
-// 		if(openCards.length === 2){
-// 			if(match === true){
-// 				openCards = [];
-// 				cardIndex = [];
-//                cards[i].classList.add('open','show');
-// 	           openCards.push(cards[i].children[0].classList.item(1));
-// 	           cardIndex.push(i);
-// 	           match = false;
-// 	         }
-// 	        else{
-
-// 	        	cards[cardIndex[0]].classList.remove('open','show');
-// 			    cards[cardIndex[1]].classList.remove('open','show');
-
-// 			    openCards = [];
-// 			    cardIndex = [] ;
-//                cards[i].classList.add('open','show');
-// 	           openCards.push(cards[i].children[0].classList.item(1));
-// 	           cardIndex.push(i);
-// 	        }
-// 		}
-// 		else if (openCards.length === 1){
-// 			cards[i].classList.add('open','show');
-// 			openCards.push(cards[i].children[0].classList.item(1));
-// 			cardIndex.push(i);
-// 			console.log(openCards[0]);
-// 			console.log(openCards[1]);
-//             if(openCards[0] === openCards[1]){
-// 			cards[i].classList.add('match');
-// 			cards[cardIndex[0]].classList.remove('open','show');
-// 			cards[cardIndex[0]].classList.add('match');
-//              match = true;
-// 		   }
-
-// 	    }
-// 	 }
-// 	else{
-// 		cards[i].classList.add('open','show');
-// 		openCards.push(cards[i].children[0].classList.item(1));
-// 		cardIndex.push(i);
-// 	}
-// })
-// }
 
 let index = 0 ;
 let openCards = [] ;
 let cardIndex = [] ;
 let match = false;
+let moves = 0 ;
 //function to display the clicked card
 function openCard(arrOfCards,index){
 	cards[index].classList.add('open','show');
@@ -177,6 +95,12 @@ function hideCards(){
     	cards[cardIndex[0]].classList.remove('open','show');
 	    cards[cardIndex[1]].classList.remove('open','show');
 }
+// function to increment number of moves
+function incrementMove(){
+	moves++;
+	const moveStatus = document.querySelector('.moves');
+    moveStatus.textContent = moves ;
+}
 for(let i = 0 ;i< cards.length ;i++){
     cards[i].addEventListener('click',function(){
 	if(openCards.length!==0){
@@ -197,8 +121,10 @@ for(let i = 0 ;i< cards.length ;i++){
 	        }
 		}
 		else if (openCards.length === 1){
+            
 			openCard(cards,i);
 			holdCards(cards , i);
+			incrementMove();
             if(openCards[0] === openCards[1]){
             	matchedCardsLock(i);
 		   }
