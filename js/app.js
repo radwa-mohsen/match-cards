094545ccc6f2d96c19145fbaedc9b7fa70c38e77
Radwa Ahmed Mohsen <radwa@gfxway.com>
2018-05-12 19:11:37 +0200

    configure new game button

--- js/app.js
+++ js/app.js
@@ -3,17 +3,11 @@
 
  */
 
-let ContentOfCards = document.querySelectorAll('.card i');
-function toArray(obj) {
-  var array = [];
-  // iterate backwards ensuring that length is an UInt32
-  for (var i = obj.length >>> 0; i--;) { 
-    array[i] = obj[i];
-  }
-  return array;
-}
-ContentOfCards = toArray(ContentOfCards);
 
+let ContentOfCards = document.querySelectorAll('.card i');
+ContentOfCards = toArray(ContentOfCards);
+let cards = document.querySelectorAll('.card');
+ cards = toArray(cards);
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -22,60 +16,15 @@ ContentOfCards = toArray(ContentOfCards);
  *   - add each card's HTML to the page
  */
 
- let cards = document.querySelectorAll('.card');
-
- cards = toArray(cards);
-//restart icon configuration
-function resetMoves(){
-	moves = 0;
-	const moveStatus = document.querySelector('.moves');
-    moveStatus.textContent = moves ;
-}
-
-function resetStars(){
-	numberOfStars = 3;
-	let stars = document.querySelectorAll('.stars li');
-	stars = toArray(stars);
-	for (let i = 0; i < numberOfStars; i++) {
-		stars[i].children[0].classList.remove('fa-star-o');
-		stars[i].children[0].classList.add('fa-star');
-	}
-}
+ 
+ 
 const restartIcon = document.querySelector('.restart')
 restartIcon.addEventListener('click',function(){
-	for(let i=0 ; i<cards.length ; i++){
-	cards[i].classList.remove('open','show','match');
-}
-resetMoves();
-resetStars();
-shuffle(ContentOfCards);
-openCards = [];
-cardIndex = [];
-clearInterval(intervalId);
-totalSeconds = 0;
-intervalId = setInterval(setTime, 1000);
+	reset();
 });
 
 //shuffle the cards
 ContentOfCards = shuffle(ContentOfCards);
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    for(let i=0 ; i<cards.length ; i++){
-	cards[i].innerHTML = ContentOfCards[i].outerHTML;
-}
-    return array;
-}
-
 
 
  // * set up the event listener for a card. If a card is clicked: done
@@ -94,65 +43,25 @@ let match = false;
 let moves = 0 ;
 let numberOfStars = 3 ;
 let numberOfMatches = 0
-//function to display the clicked card
-function openCard(arrOfCards,index){
-	cards[index].classList.add('open','show');
-}
-//function to hold the cards symbol and it's index
-function holdCards(arrOfCards,index){
-	openCards.push(cards[index].children[0].classList.item(1));
-	cardIndex.push(index);
-}
-//function to lock the cards in open position in case of matching
-function matchedCardsLock(index){
-    cards[index].classList.add('match');
-	cards[cardIndex[0]].classList.remove('open','show');
-	cards[cardIndex[0]].classList.add('match');
-     numberOfMatches++;
-     if(numberOfMatches === 8){
-     	clearInterval(intervalId);
-     	document.querySelector('.congrat').textContent = 'Congratulations';
-     	document.querySelector('.congrat').style.cssText = 'display : inline-block ; background-color:white ; position:fixed ; width:100% ; height:100%';
-     }
-}
-// function to hide not matched cards
-function hideCards(){
-    	cards[cardIndex[0]].classList.remove('open','show');
-	    cards[cardIndex[1]].classList.remove('open','show');
-}
-// function to increment number of moves
-function incrementMove(){
-	moves++;
-	const moveStatus = document.querySelector('.moves');
-    moveStatus.textContent = moves ;
-    changeStar();
-}
-function changeStar(){
-	let stars = document.querySelectorAll('.fa-star');
-    stars = toArray(stars);
-	if(moves == 9){
-	    stars[2].classList.remove('fa-star');
-	    stars[2].classList.add('fa-star-o')
-        numberOfStars--;
-	}
-	else if(moves == 17){
-		stars[1].classList.remove('fa-star');
-	    stars[1].classList.add('fa-star-o');
-	    numberOfStars--;
-	}
 
-}
+// submit button to start new game
+const newGame = document.querySelector('input');
+newGame.addEventListener('click',function(){
+	reset();
+	document.querySelector('.congrat').style.cssText = 'display : none';
+} )
 
+// listen to each click on card
 for(let i = 0 ;i< cards.length ;i++){
     cards[i].addEventListener('click',function(){
 			if(openCards.length!==0){
-				
 				if (openCards.length === 1){
-					
+					// to make sure he didn't click the same card
 				    if(cardIndex[0]!= i) {
 						openCard(cards,i);
 						holdCards(cards , i);
 						incrementMove();
+						//check if the clicked card matched with the open card or not
 			            if(openCards[0] === openCards[1]){
 			            	matchedCardsLock(i);
 			            	 openCards = [];
@@ -175,6 +84,113 @@ for(let i = 0 ;i< cards.length ;i++){
 			}
 	
 })
+}
+
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    for(let i=0 ; i<cards.length ; i++){
+	cards[i].innerHTML = ContentOfCards[i].outerHTML;
+}
+    return array;
+}
+//function to reset everything
+function reset(){
+for(let i=0 ; i<cards.length ; i++){
+	cards[i].classList.remove('open','show','match');
+}
+numberOfMatches=0;
+resetMoves();
+resetStars();
+shuffle(ContentOfCards);
+openCards = [];
+cardIndex = [];
+clearInterval(intervalId);
+totalSeconds = 0;
+intervalId = setInterval(setTime, 1000);
+}
+//function to reset the number of moves to zero
+function resetMoves(){
+	moves = 0;
+	const moveStatus = document.querySelector('.moves');
+    moveStatus.textContent = moves ;
+}
+//function to reset the number of given stars to 3
+function resetStars(){
+	numberOfStars = 3;
+	let stars = document.querySelectorAll('.stars li');
+	stars = toArray(stars);
+	for (let i = 0; i < numberOfStars; i++) {
+		stars[i].children[0].classList.remove('fa-star-o');
+		stars[i].children[0].classList.add('fa-star');
+	}
+}
+//function to convert node list to array and token from this link 
+ //:https://stackoverflow.com/questions/3836582/how-to-translate-prototypes-a-function-into-jquery/3836622#3836622
+function toArray(obj) {
+  var array = [];
+  // iterate backwards ensuring that length is an UInt32
+  for (var i = obj.length >>> 0; i--;) { 
+    array[i] = obj[i];
+  }
+  return array;
+}
+//function to display the clicked card
+function openCard(arrOfCards,index){
+	cards[index].classList.add('open','show','animated','rubberBand');
+}
+//function to hold the cards symbol and it's index
+function holdCards(arrOfCards,index){
+	openCards.push(cards[index].children[0].classList.item(1));
+	cardIndex.push(index);
+}
+//function to lock the cards in open position in case of matching
+function matchedCardsLock(index){
+    cards[index].classList.add('match');
+	cards[cardIndex[0]].classList.remove('open','show');
+	cards[cardIndex[0]].classList.add('match');
+     numberOfMatches++;
+     if(numberOfMatches === 8){
+     	clearInterval(intervalId);
+     	document.querySelector('.congrat').style.cssText = 'display : block';
+     }
+}
+// function to hide not matched cards
+function hideCards(){
+    	cards[cardIndex[0]].classList.remove('open','show','animated','rubberBand');
+	    cards[cardIndex[1]].classList.remove('open','show','animated','rubberBand');
+}
+// function to increment number of moves
+function incrementMove(){
+	moves++;
+	const moveStatus = document.querySelector('.moves');
+    moveStatus.textContent = moves ;
+    changeStar();
+}
+function changeStar(){
+	let stars = document.querySelectorAll('.fa-star');
+    stars = toArray(stars);
+	if(moves == 9){
+	    stars[2].classList.remove('fa-star');
+	    stars[2].classList.add('fa-star-o')
+        numberOfStars--;
+	}
+	else if(moves == 17){
+		stars[1].classList.remove('fa-star');
+	    stars[1].classList.add('fa-star-o');
+	    numberOfStars--;
+	}
+
 }
 
 // make a timer source of code : https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
